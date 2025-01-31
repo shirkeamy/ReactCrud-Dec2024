63b70b81053c63c59e1522df983add2630b7ca68
Amol Shirke <shirkeamy@gmail.com>
2025-01-31 12:04:42 +0530

    Call API from ui

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getAllEmployees } from "../Utils/EmployeesServices";
+import { IEmployees } from "../Utils/Interfaces";
 
 const Home: React.FC = () => {
+
+    useEffect(()=>{
+        getAllEmployees()
+            .then((data: IEmployees[])=>{
+                console.log("data", data)
+            })
+            .catch((error)=>{
+                console.error("Error from home page:", error);
+            })
+    }, [])
+    
     return (
         <>
             <h1>Home</h1>
