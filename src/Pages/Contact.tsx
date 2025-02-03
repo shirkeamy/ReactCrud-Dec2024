3531445d27521e0c5e4e8135519fd077d35090fb
Amol Shirke <shirkeamy@gmail.com>
2025-02-03 08:04:28 +0530

    Use input wrapper

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import InputWrapper from "../Components/FormComponent/InputWrapper";
 
 const Contact: React.FC = () => {
     return (
         <>
             <h1>Contact</h1>
+            <label htmlFor="txtName">Name</label>
+            <input
+                type="tel"
+                name=""
+                id="txtName"
+                className=""
+                onChange={()=>{}}
+            />
+
+            <InputWrapper labelText={"Email"} type={"text"} id={"txtEmail"} />
+            <br />
+            <InputWrapper
+                labelText={"Phone Number"}
+                type={"tel"}
+                id={"txtPhoneNumber"}
+                className="text-danger bg-primary" />
+
         </>
     )
 }
