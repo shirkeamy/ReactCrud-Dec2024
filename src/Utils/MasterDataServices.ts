9e238fe510622373786e9b21f865e85955988b3b
Amol Shirke <shirkeamy@gmail.com>
2025-02-07 08:47:35 +0530

    Add state API service

--- src/Utils/MasterDataServices.ts
+++ src/Utils/MasterDataServices.ts
@@ -1,8 +1,15 @@
-import { IContries } from "./Interfaces";
+import { IContries, IStates } from "./Interfaces";
 
 export const getContries = async (): Promise<IContries[]> => {
 
     const result = await fetch("https://localhost:7196/api/master-data/countries");
+
+    return result.json()
+}
+
+export const getStates = async (): Promise<IStates[]> => {
+
+    const result = await fetch("https://localhost:7196/api/master-data/states");
 
     return result.json()
 }
